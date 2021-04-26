3d2ad761810cb04aefed05289295a693015327be
dinhanhx <dinhanhx@gmail.com>
2021-04-26 15:31:45 +0700

    Rewrite to be suitable for ict6

--- UNITER/conda/setup_uniter.sh
+++ UNITER/conda/setup_uniter.sh
@@ -1,10 +1,9 @@
 conda create -n uniter python=3.6 pip --yes
-source ~/anaconda3/etc/profile.d/conda.sh
 conda activate uniter
 
-cd /path/to/UNITER
+cd ../
 
-conda install pytorch==1.0.1 torchvision==0.2.2 cudatoolkit=9.0 -c pytorch --yes
+conda install pytorch==1.0.1 torchvision==0.2.2 cudatoolkit=10.2 -c pytorch --yes
 conda install scikit-learn --yes
 pip install pytorch-pretrained-bert==0.6.2 tensorboardX==1.7 ipdb==0.12 lz4==2.1.9 lmdb==0.97
 pip install toolz cytoolz msgpack msgpack-numpy
@@ -15,5 +14,3 @@ HOROVOD_WITH_PYTORCH=1 pip install --no-cache-dir horovod
 git clone https://github.com/jackroos/apex
 cd ./apex
 pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-
-echo "Done"
